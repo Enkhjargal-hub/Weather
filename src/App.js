e1ab6f9cf0bb77f035b20398e8b7604d0594822e
Enkhjargal-hub <Joloenja@gmail.com>
2025-01-14 12:33:08 +0800

    j

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import './App.css';
+import "./App.css";
 import { useEffect, useState, useCallback } from "react";
 import { citiesFilter } from "./utils/CitiesFilter";
 
@@ -12,7 +11,9 @@ function App() {
   const fetchData = async () => {
     setLoading(true);
     try {
-      const response = await fetch("https://countriesnow.space/api/v0.1/countries");
+      const response = await fetch(
+        "https://countriesnow.space/api/v0.1/countries"
+      );
       const result = await response.json();
       const countriesAndCity = citiesFilter(result.data);
       setCities(countriesAndCity);
@@ -52,6 +53,62 @@ function App() {
 
   return (
     <div className="App">
+      <div>
+        {/* <img
+        className="Photo"
+        src="/img/Screenshot 2025-01-13 at 5.58.41 PM.png"
+        alt="Screenshot"
+      /> */}
+      </div>
+      <div>
+        <div className="Sun">
+          <div>
+            <h4 className="Date">January 14, 2025</h4>
+            <h2 className="World">Ulaanbaatar</h2>
+            <div>
+              <svg
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-map-pin text-gray-600"
+              >
+                <path
+                  d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"
+                  stroke="#4B5563"
+                  fill="none"
+                  stroke-width="2px"
+                ></path>
+                <circle
+                  cx="12"
+                  cy="10"
+                  r="3"
+                  stroke="#4B5563"
+                  fill="none"
+                  stroke-width="2px"
+                ></circle>
+              </svg>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="Moon">
+          <div>
+        <h4 className="Date">January 14, 2025</h4>
+        <h2 className="World">Ulaanbaatar</h2>
+        <div><svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin text-gray-600"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" stroke="#4B5563" fill="none" stroke-width="2px"></path><circle cx="12" cy="10" r="3" stroke="#4B5563" fill="none" stroke-width="2px"></circle></svg></div>
+        </div>
+        <div>
+        </div>
+        </div>
+      </div>
       {loading && <p>Loading...</p>}
       <div>
         <input
