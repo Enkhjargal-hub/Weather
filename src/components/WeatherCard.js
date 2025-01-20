0fab33b602120314852a7ae154f1240379a86588
Enkhjargal-hub <Joloenja@gmail.com>
2025-01-20 09:15:46 +0800

    j

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,8 +2,8 @@
 const WeatherCard = ({ title, photo, date, maxTemp, minTemp, condition }) => {
   return (
     <div className="weather-card">
-      <h3>{title}</h3>
-      <img src={photo} alt={title} className="weather-image" />
+      <h3 className="text-2xl font-bold mb-4">{title}</h3>
+      <img src={photo} alt={title} className="w-full w-80 object-cover rounded-t-lg" />
       <p>{date}</p>
       <p>{maxTemp}°C / {minTemp}°C</p>
       <p>{condition}</p>
